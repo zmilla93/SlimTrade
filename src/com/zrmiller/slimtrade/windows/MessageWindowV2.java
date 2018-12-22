be48e624bfbc43f02192b65c33202661e3ec391a
zmilla93 <zmilla93@gmail.com>
2018-12-21 21:26:13 -0600

    message window improvements

--- src/com/zrmiller/slimtrade/windows/MessageWindowV2.java
+++ src/com/zrmiller/slimtrade/windows/MessageWindowV2.java
@@ -5,11 +5,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -21,7 +22,7 @@ import com.zrmiller.slimtrade.TradeOffer;
 import com.zrmiller.slimtrade.buttons.BasicButton;
 import com.zrmiller.slimtrade.panels.StashHelper;
 
-public class MessageWindow extends JPanel{
+public class MessageWindowV2 extends JPanel{
 	
 	//GLOBAL
 	private static final long serialVersionUID = 1L;
@@ -51,8 +52,8 @@ public class MessageWindow extends JPanel{
 	//Buttons Top	
 	public BasicButton callbackButton;
 	public BasicButton waitButton;
-	public BasicButton stillInterestedButton;
-	public BasicButton repeatMessageButton;
+	public BasicButton stillInterested;
+	public BasicButton repeatMessage;
 	public BasicButton closeButton;
 	//Buttons Bottom
 	private BasicButton inviteToPartyButton;
@@ -76,38 +77,13 @@ public class MessageWindow extends JPanel{
 	private Timer highlighterTimer = new Timer(2500, hideHighlighter);
 	
 	
-	public MessageWindow(TradeOffer trade){
+	public MessageWindowV2(TradeOffer trade){
 		this.trade = trade;
 		 // TODO : Optimize
 		 // Size Variables
 		 
 		this.setLayout(new BorderLayout());
 		this.setPreferredSize(new Dimension(totalWidth, totalHeight));
-		
-		double nameWidthMult = 0;
-		double priceWidthMult = 0;
-		double itemWidthMult = 0;
-		double buttonTopMult = 0;
-		double buttonBotMult = 0;
-		
-		switch(trade.msgType){
-		case INCOMING_TRADE:
-			nameWidthMult = 0.5;
-			priceWidthMult = 0.3;
-			itemWidthMult = 0.8;
-			buttonTopMult = 0.2;
-			buttonBotMult = 0.2;
-			break;
-		case OUTGOING_TRADE:
-			nameWidthMult = 0.55;
-			priceWidthMult = 0.35;
-			itemWidthMult = 0.8;
-			buttonTopMult = 0.1;
-			buttonBotMult = 0.2;
-			break;
-		default:
-			break;
-		}
 		
 		//BORDER
 		borderOuter.setLayout(new FlowLayout(FlowLayout.CENTER, borderThickness, borderThickness));
@@ -128,30 +104,52 @@ public class MessageWindow extends JPanel{
 		BasicButton.height = height/2;
 		
 		//TOP PANEL
-		LayoutManager panelFlow = new FlowLayout(FlowLayout.CENTER, 0, 1);
-		JPanel topPanel = new JPanel();
-		topPanel.setLayout(Overlay.flowCenter);
+		topPanel = new JPanel();
+		topPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
 		topPanel.setPreferredSize(new Dimension(width, rowHeight));
 		topPanel.setBackground(Color.CYAN);
 		container.add(topPanel, BorderLayout.PAGE_START);
 		
-		//NAME
-		namePanel = new JPanel(panelFlow);		
+		namePanel = new JPanel();
+		//TODO : MODIFY TO CENTER TEXT VERITCALLY
+		namePanel.setLayout(new FlowLayout(FlowLayout.LEFT, borderThickness, borderThickness));
 		nameLabel = new JLabel(trade.playerName);
-		namePanel.setPreferredSize(new Dimension((int)(width*nameWidthMult), rowHeight));
+		namePanel.setPreferredSize(new Dimension((int)(width*0.4), rowHeight));
+		nameLabel.setPreferredSize(new Dimension((int)(width*0.4), rowHeight));
 		namePanel.add(nameLabel);
 		topPanel.add(namePanel);
-		//PRICE
+		
 		pricePanel = new JPanel();
 		priceLabel = new JLabel(trade.priceType.toString());
-		pricePanel.setPreferredSize(new Dimension((int)(width*priceWidthMult), rowHeight));
+		pricePanel.setPreferredSize(new Dimension((int)(width*0.4), rowHeight));
 		pricePanel.add(priceLabel);
 		topPanel.add(pricePanel);
-		//TOP BUTTON PANEL
+		
 		topButtonPanel = new JPanel();
 		topButtonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
-		topButtonPanel.setPreferredSize(new Dimension((int)(width*buttonTopMult), rowHeight));
+		topButtonPanel.setPreferredSize(new Dimension((int)(width*0.2), rowHeight));
 		topPanel.add(topButtonPanel);
+		
+		BasicButton button2 = new BasicButton();
+		button2.setPreferredSize(new Dimension(rowHeight, rowHeight));
+		topButtonPanel.add(button2);
+		
+		BasicButton.width = rowHeight;
+		BasicButton.height = rowHeight;
+		closeButton = new BasicButton();
+		closeButton.setCustomIcon("/close.png");
+		closeButton.setBorderPresets(BorderFactory.createEmptyBorder(1, 1, 1, 1), BorderFactory.createLineBorder(Color.BLACK));
+		
+//		closeButton.setPreferredSize(new Dimension(rowHeight, rowHeight));
+		
+		
+//		File img = new File("/images/icons/close.png");
+//		System.out.println(img.exists());
+//		priceIcon.setIcon(new ImageIcon(new ImageIcon(this.getClass().getResource("/" + trade.priceType + ".png")).getImage().getScaledInstance(height/2, height/2, Image.SCALE_SMOOTH)));
+//		closeButton.setIcon((Icon) priceIcon);
+//		priceIcon.setBounds(0, 0, ref.priceWidth, ref.priceHeight);
+		
+		topButtonPanel.add(closeButton);
 		
 		/*
 		 * CENTER PANEL
@@ -164,24 +162,19 @@ public class MessageWindow extends JPanel{
 		
 		itemPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
 		itemLabel = new JLabel(trade.itemName);
-		itemPanel.setPreferredSize(new Dimension((int)(width*itemWidthMult), rowHeight));
+		itemPanel.setPreferredSize(new Dimension((int)(width*0.8), rowHeight));
 		itemPanel.add(itemLabel, BorderLayout.CENTER);
 		centerPanel.add(itemPanel);
 		
 		JPanel centerButtonPanel = new JPanel();
 		centerButtonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
-		centerButtonPanel.setPreferredSize(new Dimension((int)(width*buttonBotMult), rowHeight));
+		centerButtonPanel.setPreferredSize(new Dimension((int)(width*0.2), rowHeight));
 		centerPanel.add(centerButtonPanel);
 		
 		//Buttons
 		BasicButton.width = rowHeight;
 		BasicButton.height = rowHeight;
 		//Row 1
-		callbackButton = new BasicButton();
-		waitButton = new BasicButton();
-		stillInterestedButton = new BasicButton();
-		repeatMessageButton = new BasicButton();
-		closeButton = new BasicButton("/close.png");
 		//Row 2
 		inviteToPartyButton = new BasicButton("/invite.png");
 		tpToHideoutButton = new BasicButton("/warp.png");
@@ -191,24 +184,14 @@ public class MessageWindow extends JPanel{
 		leavePartyButton = new BasicButton("/leave.png");
 		tpHomeButton = new BasicButton("/home.png");
 		
-		
-		
 		switch(trade.msgType){
 		case INCOMING_TRADE:
-			topButtonPanel.add(callbackButton);
-			topButtonPanel.add(waitButton);
-			topButtonPanel.add(stillInterestedButton);
-			topButtonPanel.add(closeButton);
-			//
 			centerButtonPanel.add(inviteToPartyButton);
 			centerButtonPanel.add(tradeButton);
 			centerButtonPanel.add(thankButton);
 			centerButtonPanel.add(kickButton);
 			break;
-		case OUTGOING_TRADE:
-			topButtonPanel.add(repeatMessageButton);
-			topButtonPanel.add(closeButton);
-			//
+		case OUTGOING_TRADE: 
 			centerButtonPanel.add(tpToHideoutButton);
 			centerButtonPanel.add(tradeButton);
 			centerButtonPanel.add(thankButton);
@@ -220,7 +203,6 @@ public class MessageWindow extends JPanel{
 		case UNKNOWN:
 			break;
 		}
-		
 		
 		Border blankBorder = BorderFactory.createEmptyBorder(1, 1, 1, 1);
 		Border hoverBorder = BorderFactory.createLineBorder(Color.RED);
