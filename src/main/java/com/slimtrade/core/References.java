e20ee2bca26bd22fc11b9371823baab5dd30dcb5
zmilla93 <zmilla93@gmail.com>
2019-03-13 23:11:54 -0500

    version

--- src/main/java/com/slimtrade/core/References.java
+++ src/main/java/com/slimtrade/core/References.java
@@ -2,7 +2,6 @@ package main.java.com.slimtrade.core;
 
 public class References {
 
-//	public static final String APP_VERSION = "0.1.1";
-	public static final String APP_VERSION = "0.0.0";
+	public static final String APP_VERSION = "0.1.1";
 	
 }
