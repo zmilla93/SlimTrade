d68f6c708c925967ca5492864db052f283a4cffb
zmilla93 <zmilla93@gmail.com>
2020-02-19 19:04:24 -0600

    updated version number

--- src/main/java/com/slimtrade/core/References.java
+++ src/main/java/com/slimtrade/core/References.java
@@ -3,7 +3,7 @@ package com.slimtrade.core;
 public class References {
 
 	public static final String APP_NAME = "SlimTrade";
-	public static final String APP_VERSION = "0.1.4";
+	public static final String APP_VERSION = "0.1.5";
 	public static final int DEFAULT_IMAGE_SIZE = 18;
 
 	public static final String POE_WINDOW_TITLE = "Path of Exile";
