34a34f034a225375729c8b0215ea02d5a66fa3f8
zmilla93 <zmilla93@gmail.com>
2020-04-03 09:51:14 -0500

    updated version text

--- src/main/java/com/slimtrade/gui/windows/UpdateDialog.java
+++ src/main/java/com/slimtrade/gui/windows/UpdateDialog.java
@@ -21,7 +21,7 @@ public class UpdateDialog extends JFrame implements IColorable {
     private JButton viewUpdateButton = new ConfirmButton("View on Github");
 
     private JLabel info1 = new CustomLabel("Update Available!");
-    private JLabel info2 = new CustomLabel("Currently Running: " + References.getAppVersion());
+    private JLabel info2 = new CustomLabel("Currently Running: " + App.updateChecker.getCurrentVersion());
     private JLabel info3 = new CustomLabel("Latest Version: " + App.updateChecker.getLatestRelease());
 
     private final int BUFFER = 30;
