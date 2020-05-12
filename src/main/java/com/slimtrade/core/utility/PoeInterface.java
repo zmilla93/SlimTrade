ec4b56dd0ba89b6225192a316d6995d4f52fa02a
zmilla93 <zmilla93@gmail.com>
2020-05-12 17:28:28 -0500

    stash search dialog is now automatically unfocused

--- src/main/java/com/slimtrade/core/utility/PoeInterface.java
+++ src/main/java/com/slimtrade/core/utility/PoeInterface.java
@@ -307,6 +307,8 @@ public class PoeInterface extends Robot {
         robot.keyPress(KeyEvent.VK_V);
         robot.keyRelease(KeyEvent.VK_V);
         robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
         FrameManager.forceAllToTop();
     }
 
