99177568be3484349747328ebf4d09d89d04a2f6
zmilla93 <zmilla93@gmail.com>
2020-09-05 16:07:17 -0500

    downgraded jnativehook
    jnativehook v2.1.0 was causing issues with international keyboard layouts that seem to not be present in v2.0.3 and previous.

--- src/main/java/com/slimtrade/core/observing/GlobalKeyboardListener.java
+++ src/main/java/com/slimtrade/core/observing/GlobalKeyboardListener.java
@@ -7,33 +7,33 @@ import org.jnativehook.keyboard.NativeKeyListener;
 public class GlobalKeyboardListener implements NativeKeyListener {
 
     private HotkeyListener hotkeyListener = null;
-    // Masks for the 5 mouse buttons
-    private static final int[] removeMasks = {256, 512, 1024, 2048, 4096};
 
     private static volatile boolean ctrlPressed;
     private static volatile boolean atlPressed;
     private static volatile boolean shiftPressed;
+    // Masks for the 5 mouse buttons
+    private static final int[] mouseMasks = {256, 512, 1024, 2048, 4096};
 
     @Override
     public void nativeKeyPressed(NativeKeyEvent e) {
         // Remove mouse button modifiers
         e.setModifiers(cleanModifiers(e.getModifiers()));
 
-        if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL) {
+        if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL_L || e.getKeyCode() == NativeKeyEvent.VC_CONTROL_R) {
             ctrlPressed = true;
-        } else if (e.getKeyCode() == NativeKeyEvent.VC_ALT) {
+        } else if (e.getKeyCode() == NativeKeyEvent.VC_ALT_L || e.getKeyCode() == NativeKeyEvent.VC_ALT_R) {
             atlPressed = true;
-        } else if (e.getKeyCode() == NativeKeyEvent.VC_SHIFT) {
+        } else if (e.getKeyCode() == NativeKeyEvent.VC_SHIFT_L || e.getKeyCode() == NativeKeyEvent.VC_SHIFT_L) {
             shiftPressed = true;
         }
         // Ignore modifier keys on their own
-        if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL
-                || e.getKeyCode() == NativeKeyEvent.VC_ALT
-                || e.getKeyCode() == NativeKeyEvent.VC_SHIFT
+        if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL_L || e.getKeyCode() == NativeKeyEvent.VC_CONTROL_R
+                || e.getKeyCode() == NativeKeyEvent.VC_ALT_L || e.getKeyCode() == NativeKeyEvent.VC_ALT_R
+                || e.getKeyCode() == NativeKeyEvent.VC_SHIFT_L || e.getKeyCode() == NativeKeyEvent.VC_SHIFT_R
                 || e.getKeyCode() == NativeKeyEvent.VC_NUM_LOCK
                 || e.getKeyCode() == NativeKeyEvent.VC_SCROLL_LOCK
                 || e.getKeyCode() == NativeKeyEvent.VC_CAPS_LOCK
-                || e.getKeyCode() == NativeKeyEvent.VC_META
+                || e.getKeyCode() == NativeKeyEvent.META_L_MASK ||  e.getKeyCode() == NativeKeyEvent.META_R_MASK
                 || e.getKeyCode() == NativeKeyEvent.VC_UNDEFINED
                 || NativeKeyEvent.getKeyText(e.getKeyCode()).contains("Unknown")) {
             return;
@@ -57,11 +57,11 @@ public class GlobalKeyboardListener implements NativeKeyListener {
 
     @Override
     public void nativeKeyReleased(NativeKeyEvent e) {
-        if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL) {
+        if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL_L || e.getKeyCode() == NativeKeyEvent.VC_CONTROL_R) {
             ctrlPressed = false;
-        } else if (e.getKeyCode() == NativeKeyEvent.VC_ALT) {
+        } else if (e.getKeyCode() == NativeKeyEvent.VC_ALT_L || e.getKeyCode() == NativeKeyEvent.VC_ALT_R) {
             atlPressed = false;
-        } else if (e.getKeyCode() == NativeKeyEvent.VC_SHIFT) {
+        } else if (e.getKeyCode() == NativeKeyEvent.VC_SHIFT_L || e.getKeyCode() == NativeKeyEvent.VC_SHIFT_R) {
             shiftPressed = false;
         }
     }
@@ -106,15 +106,15 @@ public class GlobalKeyboardListener implements NativeKeyListener {
 
     // Removes the 5 mouse buttons as modifiers for key events
     public static int cleanModifiers(int mods) {
-        for (int mask : removeMasks) {
+        for (int mask : mouseMasks) {
             if ((mods & mask) > 0) {
                 mods -= mask;
             }
         }
         // Ctrl fix
-        if ((mods & 34) > 0 && !ctrlPressed) {
-            mods -= 2;
-        }
+//        if ((mods & 34) > 0 && !ctrlPressed) {
+//            mods -= 2;
+//        }
         return mods;
     }
 
