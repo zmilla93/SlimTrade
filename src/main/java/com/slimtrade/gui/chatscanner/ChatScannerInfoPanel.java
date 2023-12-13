459dcde2eae132fa4ee5156e4293b2719bb72feb
Zach Miller <zmilla93@gmail.com>
2023-12-12 23:48:46 -0600

    updated scanner info

--- src/main/java/com/slimtrade/gui/chatscanner/ChatScannerInfoPanel.java
+++ src/main/java/com/slimtrade/gui/chatscanner/ChatScannerInfoPanel.java
@@ -9,15 +9,11 @@ public class ChatScannerInfoPanel extends AbstractOptionPanel {
 
     public ChatScannerInfoPanel() {
         super(true);
-        addHeader("Chat Scanner Overview");
+        addHeader("Chat Scanner Info");
         addPanel(new PlainLabel("Allows you to search for custom phrases in chat."));
         addPanel(new PlainLabel("Once you create an entry, select it from the list then click 'Start Scanning'."));
-        addPanel(new JLabel("Click 'New Entry' in the lower left to get started!"));
-        addVerticalStrut();
-        addHeader("Useful Tips");
         addPanel(new PlainLabel("Use CTRL or SHIFT to select multiple entries for scanning."));
-        addPanel(new PlainLabel("The scanner button on the menubar can be right clicked to toggle scanning."));
-        addPanel(new PlainLabel("Changes are auto-saved when you start scanning."));
+        addPanel(new JLabel("Click 'New Entry' in the lower left to get started!"));
     }
 
 }
