7444ec208f7a981ce7f382acb40d3cdfff04308f
Zach Miller <zmilla93@gmail.com>
2023-12-20 17:44:58 -0600

    Update AudioRowControls.java

--- src/main/java/com/slimtrade/gui/options/audio/AudioRowControls.java
+++ src/main/java/com/slimtrade/gui/options/audio/AudioRowControls.java
@@ -2,16 +2,18 @@ package com.slimtrade.gui.options.audio;
 
 import com.slimtrade.core.audio.Sound;
 import com.slimtrade.core.audio.SoundComponent;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
 public record AudioRowControls(JComboBox<Sound> comboBox, JSlider slider) {
 
-    public void setSoundComponent(SoundComponent component) {
-        this.comboBox.setSelectedItem(component.sound);
-        this.slider.setValue(component.volume);
+    public void setSoundComponent(@NotNull SoundComponent component) {
+        comboBox.setSelectedItem(component.sound);
+        slider.setValue(component.volume);
     }
 
+    @NotNull
     public SoundComponent getSoundComponent() {
         Sound sound = (Sound) comboBox.getSelectedItem();
         int volume = slider.getValue();
