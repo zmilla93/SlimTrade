f20a6dc941b98fa294ac77d237f2ca1879e6d858
Zach Miller <zmilla93@gmail.com>
2023-12-27 19:34:12 -0600

    improved macro customizer layout

--- src/main/java/com/slimtrade/gui/options/MacroCustomizerPanel.java
+++ src/main/java/com/slimtrade/gui/options/MacroCustomizerPanel.java
@@ -29,50 +29,42 @@ public class MacroCustomizerPanel extends AddRemovePanel {
     private final HotkeyButton hotkeyButton = new HotkeyButton();
 
     // Internal
-    private final JPanel parent;
     private final CardLayout displayLayout = new CardLayout();
     private final JPanel displayPanel = new JPanel(displayLayout);
     private final JButton shiftUpButton = new IconButton(DefaultIcon.ARROW_UP.path);
     private final JButton shiftDownButton = new IconButton(DefaultIcon.ARROW_DOWN.path);
     private final JButton deleteButton = new IconButton(DefaultIcon.CLOSE.path);
-    private final GridBagConstraints gc = ZUtil.getGC();
 
-    public MacroCustomizerPanel(AddRemoveContainer parent) {
+    public MacroCustomizerPanel(AddRemoveContainer<?> parent) {
         super(parent);
-        this.parent = parent;
         setLayout(new GridBagLayout());
 
         // Labels
         JLabel lmbLabel = new JLabel("LMB");
         JLabel rmbLabel = new JLabel("RMB");
-        JLabel closeLabel = new JLabel("Close");
         ThemeManager.addStickyCombo(iconCombo);
         buttonText.setText("~");
-
-        // Card Type Panel
-
         displayPanel.add(iconCombo, MacroButtonType.ICON.toString());
         displayPanel.add(buttonText, MacroButtonType.TEXT.toString());
 
-        gc.insets = new Insets(0, 5, 0, 5);
-
         // Add items to combos
-        for (ButtonRow row : ButtonRow.values())
-            rowCombo.addItem(row);
-        for (CustomIcon icon : CustomIcon.values())
-//            iconCombo.addItem(icon.getColorIcon(UIManager.getColor("Label.foreground"), 18));
-            iconCombo.addItem(ThemeManager.getColorIcon(icon.path));
-        for (MacroButtonType type : MacroButtonType.values())
-            buttonType.addItem(type);
+        for (ButtonRow row : ButtonRow.values()) rowCombo.addItem(row);
+        for (CustomIcon icon : CustomIcon.values()) iconCombo.addItem(ThemeManager.getColorIcon(icon.path));
+        for (MacroButtonType type : MacroButtonType.values()) buttonType.addItem(type);
 
-        // Shift Buttons
+        // Delete & Shift Buttons
+        GridBagConstraints gc = ZUtil.getGC();
+        add(deleteButton, gc);
+        gc.gridx++;
+        gc.insets = new Insets(0, 0, 0, 2);
         add(shiftUpButton, gc);
         gc.gridy++;
         add(shiftDownButton, gc);
         gc.gridx++;
         gc.gridy = 0;
+        gc.insets.right = 0;
 
-        //Labels
+        // Mouse Labels
         add(lmbLabel, gc);
         gc.gridy++;
         add(rmbLabel, gc);
@@ -86,17 +78,7 @@ public class MacroCustomizerPanel extends AddRemovePanel {
         gc.gridx++;
         gc.gridy = 0;
 
-        // Icon  + Row
-//        gc.fill = GridBagConstraints.HORIZONTAL;
-//        gc.weightx = 1;
-//        add(iconCombo, gc);
-//        gc.gridy++;
-//        add(rowCombo, gc);
-//        gc.gridx++;
-//        gc.gridy = 0;
-
-        // Icon  + Row
-
+        // Button type & icon/text
         gc.fill = GridBagConstraints.HORIZONTAL;
         gc.weightx = 1;
         add(buttonType, gc);
@@ -104,41 +86,26 @@ public class MacroCustomizerPanel extends AddRemovePanel {
         add(displayPanel, gc);
         gc.gridx++;
         gc.gridy = 0;
+
+        // Row & Hotkey
         add(rowCombo, gc);
         gc.gridy++;
         add(new ButtonWrapper(hotkeyButton), gc);
         gc.gridx++;
-        add(closeCheckbox, gc);
         gc.gridy = 0;
+        add(closeCheckbox, gc);
         gc.gridx++;
-//        gc.gridx++;
-//        gc.gridy = 0;
-
-        // Close
-//        gc.fill = GridBagConstraints.NONE;
-//        gc.weightx = 1;
-//        add(closeLabel, gc);
-//        gc.gridy++;
-//        add(closeCheckbox, gc);
-//        gc.gridx++;
-//        gc.gridy = 0;
-
-        // Delete Button
-        gc.fill = GridBagConstraints.HORIZONTAL;
-        add(deleteButton, gc);
 
         updateUI();
         addListeners();
     }
 
     private void addListeners() {
-        JPanel self = this;
         shiftUpButton.addActionListener(e -> shiftUp(shiftUpButton));
         shiftDownButton.addActionListener(e -> shiftDown(shiftDownButton));
         deleteButton.addActionListener(e -> {
+            removeFromParent();
             ThemeManager.removeStickyCombo(iconCombo);
-            parent.remove(self);
-            parent.revalidate();
         });
         buttonType.addActionListener(e -> {
             if (buttonType.getSelectedItem() == null) return;
@@ -179,7 +146,6 @@ public class MacroCustomizerPanel extends AddRemovePanel {
             int selectedIndex = iconCombo.getSelectedIndex();
             iconCombo.removeAllItems();
             for (CustomIcon icon : CustomIcon.values()) {
-//                iconCombo.addItem(icon.getColorIcon(UIManager.getColor("Label.foreground"), SaveManager.settingsSaveFile.data.iconSize));
                 iconCombo.addItem(ThemeManager.getColorIcon(icon.path));
             }
             iconCombo.setSelectedIndex(selectedIndex);
