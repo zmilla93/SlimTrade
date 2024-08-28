784990bfb5807448ea7f95d7dab7aacf6ac91c24
zmilla93 <zmilla93@gmail.com>
2024-08-28 05:38:41 -0500

    fixed audio list separator color

--- src/main/java/com/slimtrade/gui/components/AudioListCellRenderer.java
+++ src/main/java/com/slimtrade/gui/components/AudioListCellRenderer.java
@@ -21,9 +21,11 @@ public class AudioListCellRenderer implements ListCellRenderer<Sound> {
         if (isSelected) {
             label.setBackground(list.getSelectionBackground());
             label.setForeground(list.getSelectionForeground());
+            separator.setForeground(list.getSelectionForeground());
         } else {
             label.setBackground(list.getBackground());
             label.setForeground(list.getForeground());
+            separator.setForeground(list.getForeground());
         }
         return label;
     }
