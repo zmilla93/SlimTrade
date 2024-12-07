9fb5e71eb10a729a728e573e8b776121a5e21ced
zmilla93 <zmilla93@gmail.com>
2024-12-07 16:39:45 -0600

    marked kalguur calculator as deprecated

--- src/main/java/com/slimtrade/gui/options/KalguurOptionPanel.java
+++ src/main/java/com/slimtrade/gui/options/KalguurOptionPanel.java
@@ -1,10 +1,14 @@
 package com.slimtrade.gui.options;
 
+import com.slimtrade.gui.components.ErrorLabel;
 import com.slimtrade.gui.options.general.KalguurSettingsPanel;
 
 public class KalguurOptionPanel extends AbstractOptionPanel {
 
     public KalguurOptionPanel() {
+        addHeader("Deprecated");
+        addComponent(new ErrorLabel("This feature is for POE 1's Settlers of Kalguur league and is likely to be removed in the future."));
+        addVerticalStrut();
         addHeader("Kalguur Helper");
         addComponent(new KalguurSettingsPanel());
     }
